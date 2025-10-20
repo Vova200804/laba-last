9d79447b176470ddd1e35bd5690e815f605117c5
Vova200804 <sanek_sanek_24@mail.ru>
2025-10-20 10:02:07 +0700

    Update and rename asd to asd.cpp

--- asd.cpp
+++ asd.cpp
@@ -4,37 +4,48 @@
 *********************************/
 
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
 int main() {
-    
     const int n = 5;
-    double G[n] = {10.0, 20.0, 30.0, 40.0, 50.0};
-    double H[n] = {2.0, 4.0, 5.0, 8.0, 10.0};
-    double F[n];
-    
+    vector<double> G(n);
+    vector<double> H(n);
+    vector<double> F(n);
+
+    cout << "Enter " << n << " element for massiv G:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> G[i];
+    }
+
+    cout << "Enter " << n << " elements for  H:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> H[i];
+    }
+
     double sumG = 0, sumH = 0, sumF = 0;
-    
+
     for (int i = 0; i < n; i++) {
         F[i] = G[i] / H[i];
-        
+
         sumG += G[i];
         sumH += H[i];
         sumF += F[i];
     }
-    
+
     cout << "mG:";
     for (int i = 0; i < n; i++) cout << G[i] << " ";
-    
+
     cout << "\nmGH:";
     for (int i = 0; i < n; i++) cout << H[i] << " ";
- 
+
     cout << "\nmF(G/H):";
     for (int i = 0; i < n; i++) cout << F[i] << " ";
-    
-    cout << "\n\n" << sumG;
-    cout << "\nH:"  << sumH;
+
+    cout << "\n\nG:" << sumG;
+    cout << "\nH:" << sumH;
     cout << "\nF:" << sumF;
-    
+
     return 0;
 }
